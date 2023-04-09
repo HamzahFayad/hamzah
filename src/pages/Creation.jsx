3471a12af610a45c81e072d8cfb7e3e6877ee127
Hamzah Fayad <hamzahfayad99@gmail.com>
2023-04-09 13:05:09 +0200

    swiper

--- src/pages/Creation.jsx
+++ src/pages/Creation.jsx
@@ -1,5 +1,10 @@
 import "../App.css"
-import { InstagramEmbed } from 'react-social-media-embed';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar } from 'swiper';
+
+import 'swiper/css';
+import { visuals } from "./creation-data"
+
 
 function Creation() {
   return (
@@ -9,24 +14,41 @@ function Creation() {
          <div className="Round Round2">
         </div> 
         </div>
-      <div className="Grid-width-wide">
+      <div className="Slide Grid-width-wide">
         <hr></hr>
         <h2 className="Subheadline Text-color">Book I - Cinematic Streets</h2>
-        <div style={{ padding: '2rem 0', display: 'flex', flexFlow: 'row wrap', gap: '2rem', justifyContent: 'center' }}>
-        <InstagramEmbed url="https://www.instagram.com/p/CpxdIFBM8vc" width={350} />
-        <InstagramEmbed url="https://www.instagram.com/p/CqH482DMESM" width={350} />
-        <InstagramEmbed url="https://www.instagram.com/p/CppicAqMBX3" width={350} />
-
-        <InstagramEmbed url="https://www.instagram.com/p/CqNNo_UMNZl/" width={350} />
-        <InstagramEmbed url="https://www.instagram.com/p/CqfJMrnMS3W/" width={350} />
-        <InstagramEmbed url="https://www.instagram.com/p/Cp9zBF4Mi1A/" width={350} />
-        </div>
+        <Swiper
+          spaceBetween={5}
+          slidesPerView={3}
+          modules={[Navigation, Pagination, Scrollbar]}
+          navigation={true}
+          pagination={{ clickable: true }}
+          breakpoints={{
+            320: {
+              slidesPerView: 1
+            },
+            900: {
+              slidesPerView: 2
+            },
+            1024: {
+              slidesPerView: 3
+            },
+          }}
+        >
+          {
+            visuals.map((visual) => {
+              return(
+                <SwiperSlide key={visual.id}><img src={visual.img} alt={visual.img} /></SwiperSlide>
+              )
+            })
+          }
+    </Swiper>
       </div>
-      <div className="Grid-width-wide">
+      <div className="Slide Grid-width-wide">
         <hr></hr>
         <h2 className="Subheadline Text-color">Book II - Busy Market</h2>
       </div>
-      <div className="Grid-width-wide">
+      <div className="Slide Grid-width-wide">
         <hr></hr>
         <h2 className="Subheadline Text-color">Cinematic Film</h2>
       </div>
---
+++ src/pages/creation-data.js
@@ -0,0 +1,38 @@
+export const visuals = [
+  {
+    id: 1,
+    img: "https://hamzahfayad.github.io/photos-collection/assets/img01.jpg",
+  },
+  {
+    id: 2,
+    img: "https://hamzahfayad.github.io/photos-collection/assets/img02.jpg",
+  },
+  {
+    id: 3,
+    img: "https://hamzahfayad.github.io/photos-collection/assets/img03.jpg",
+  },
+  {
+    id: 4,
+    img: "https://hamzahfayad.github.io/photos-collection/assets/img04.jpg",
+  },
+  {
+    id: 5,
+    img: "https://hamzahfayad.github.io/photos-collection/assets/img05.jpg",
+  },
+  {
+    id: 6,
+    img: "https://hamzahfayad.github.io/photos-collection/assets/img06.jpg",
+  },
+  {
+    id: 7,
+    img: "https://hamzahfayad.github.io/photos-collection/assets/img07.jpg",
+  },
+  {
+    id: 8,
+    img: "https://hamzahfayad.github.io/photos-collection/assets/img08.jpg",
+  },
+  {
+    id: 9,
+    img: "https://hamzahfayad.github.io/photos-collection/assets/img09.jpg",
+  },
+];
